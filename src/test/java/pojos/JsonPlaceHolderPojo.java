aabb878885094abb4a377d7d0c29d44ac2ae6076
Omer Buyukergene <buyukergene@gotmail.com>
2023-03-22 10:07:56 +0300

    22.03.2023

---
+++ src/test/java/pojos/JsonPlaceHolderPojo.java
@@ -0,0 +1,52 @@
+package pojos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JsonPlaceHolderPojo {
+    private Integer UserId;
+    private String title;
+    private Boolean completed;
+
+    public JsonPlaceHolderPojo() {
+    }
+
+    public JsonPlaceHolderPojo(Integer userId, String title, Boolean completed) {
+        UserId = userId;
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public Integer getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(Integer userId) {
+        UserId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "JsonPlaceHolderPojo{" +
+                "UserId=" + UserId +
+                ", title='" + title + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
+}
---
+++ src/test/java/post_requests/Post03.java
@@ -0,0 +1,27 @@
+package post_requests;
+
+import base_urls.JsonPlaceHolderBaseUrl;
+
+public class Post03 extends JsonPlaceHolderBaseUrl {
+ /*
+         Given
+            https://jsonplaceholder.typicode.com/todos
+            {
+            "userId": 55,
+            "title": "Tidy your room",
+            "completed": false
+            }
+        When
+            I send POST Request to the Url
+        Then
+            Status code is 201
+        And
+            response body is like {
+                                    "userId": 55,
+                                    "title": "Tidy your room",
+                                    "completed": false,
+                                    "id": 201
+                                    }
+     */
+
+}
